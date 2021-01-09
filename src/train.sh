eddbf49edb32f0aaeae732dbc14bf36e7d92fc4b
Hao <hchen069@ucr.edu>
2021-01-08 21:02:49 -0800

    src

---
+++ src/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python joint_train.py 1 major
+#python joint_train.py 2 major
+#python joint_train.py 3 major
+#python joint_train.py 4 major
+#python joint_train.py 5 major
+#python joint_train.py 6 major
+#python joint_train.py 7 major
+#python joint_train.py 8 major
+#python joint_train.py 9 major
+#python joint_train.py 10 major
+#python joint_train.py 11 major
+#python joint_train.py 12 major
+#python joint_train.py 13 brain
+#python joint_train.py 14 brain
+#python joint_train.py 15 brain
