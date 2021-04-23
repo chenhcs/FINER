556b9fbfe4701eb1e1ffe95ad7f47bfb29970d82
Hao <hchen069@ucr.edu>
2021-04-22 18:29:39 -0700

    update

--- preprocessing/data_preprocess.sh
+++ preprocessing/data_preprocess.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-folder=$1
 
-python generate_go_hierarchy.py $folder
-python seq_dataset_human.py $folder major
-python seq_dataset_human.py $folder brain
-python domain_dataset.py $folder major
-python domain_dataset.py $folder brain
-python construct_coexp_net.py $folder major
-python construct_coexp_net.py $folder brain
+python generate_go_hierarchy.py data
+python seq_dataset_human.py data major
+python seq_dataset_human.py data brain
+python domain_dataset.py data major
+python domain_dataset.py data brain
+python construct_coexp_net.py data major
+python construct_coexp_net.py data brain
